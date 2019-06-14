cd44c1dbd348b5b19a38aa73a37b3de11bbda63d
daizijian <1049384233@qq.com>
2019-06-14 10:27:58 +0800

    dzj

---
+++ build.sh
@@ -0,0 +1,13 @@
+git fetch origin
+git merge origin/master
+
+pip3 install -r requirements.txt
+
+cd backend
+python3 manage.py migrate --settings=backend.settings_prod
+python3 manage.py collectstatic --no-input --settings=backend.settings_prod
+
+supervisorctl restart video
+
+service nginx restart
+
