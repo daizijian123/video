fc236c7b760c695f82e25086324c0fc8b328504b
daizijian <1049384233@qq.com>
2019-06-14 09:57:09 +0800

    dzj

--- video/video/settings_prod.py
+++ video/video/settings_prod.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'z6toxy0cr2q2lq$ertf_f)_(n*jwocjn$cf^#tm+k8att94u%3'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
