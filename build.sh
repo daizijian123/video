4e62a66b2be646b7d4cdfaebfcced8adc62c5dda
daizijian <1049384233@qq.com>
2019-06-14 10:34:44 +0800

    dzj

--- build.sh
+++ build.sh
@@ -4,8 +4,8 @@ git merge origin/master
 pip3 install -r requirements.txt
 
 cd video
-python3 manage.py migrate --settings=backend.settings_prod
-python3 manage.py collectstatic --no-input --settings=backend.settings_prod
+python3 manage.py migrate --settings=video.settings_prod
+python3 manage.py collectstatic --no-input --settings=video.settings_prod
 
 supervisorctl restart video
 
